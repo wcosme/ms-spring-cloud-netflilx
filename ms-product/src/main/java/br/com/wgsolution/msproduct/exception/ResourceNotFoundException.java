a22552d888acdec7fbd96369d1806de751a76f50
Wallace Gonçalves <wallacecosme@gmail.com>
2021-12-10 19:25:57 -0300

    Implementando o CRUD do productService

---
+++ ms-product/src/main/java/br/com/wgsolution/msproduct/exception/ResourceNotFoundException.java
@@ -0,0 +1,14 @@
+package br.com.wgsolution.msproduct.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+	private static final long serialVersionUID = 5255653883574974078L;
+	
+	public ResourceNotFoundException(String ex) {
+		super(ex);
+	}
+
+}
--- ms-product/src/main/java/br/com/wgsolution/msproduct/services/impl/ProductServiceImpl.java
+++ ms-product/src/main/java/br/com/wgsolution/msproduct/services/impl/ProductServiceImpl.java
@@ -1,11 +1,15 @@
 package br.com.wgsolution.msproduct.services.impl;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import br.com.wgsolution.msproduct.entity.Product;
 import br.com.wgsolution.msproduct.entity.dto.ProductDTO;
+import br.com.wgsolution.msproduct.exception.ResourceNotFoundException;
 import br.com.wgsolution.msproduct.repository.ProductRepository;
 import br.com.wgsolution.msproduct.services.ProductService;
 
@@ -21,8 +25,36 @@ public class ProductServiceImpl implements ProductService {
 		
 	}
 	
-	public Page<ProductDTO> findAll(){
+	public Page<ProductDTO> findAll(Pageable pageable){
+		var page = productRepository.findAll(pageable);
 		
+		return page.map(this::convertToProductDTO);
+	}
+	
+	public ProductDTO findById(Long id) {
+		var entity = productRepository.findById(id)
+			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		
+		return ProductDTO.createProduct(entity);
+	}
+	
+	public ProductDTO update(ProductDTO productDTO) {
+		final Optional<Product> optional = productRepository.findById(productDTO.getId());
+		
+		if(!optional.isPresent()) {
+			new ResourceNotFoundException("No records found for this ID");
+		}		
+		return ProductDTO.createProduct(productRepository.save(Product.createProduct(productDTO)));
+	}
+	
+	public void delete(Long id) {
+		var entity = productRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID"));
+		productRepository.delete(entity);
+	}
+
+	private ProductDTO convertToProductDTO(Product product) {
+		return ProductDTO.createProduct(product);
 	}
 
 }
