8a7ce4d5443e850dcc26037a227faacf2e322b95
Wallace Gonçalves <wallacecosme@gmail.com>
2021-12-10 14:50:41 -0300

    Setup inicial do projeto

---
+++ ms-product/src/main/java/br/com/wgsolution/msproduct/entity/Product.java
@@ -0,0 +1,34 @@
+package br.com.wgsolution.msproduct.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_PRODUCT")
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name", nullable = false, length = 100)
+	private String name;
+	
+	@Column(name = "inventory", nullable = false, length = 10)
+	private String inventory;
+	
+	@Column(name = "price", nullable = false, length = 10)
+	private Double price;
+
+}
---
+++ ms-product/src/test/java/br/com/wgsolution/msproduct/MsProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wgsolution.msproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
