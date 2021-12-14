b6c3ed09c032bcd0855aa8989f2ecb63aba91d0b
Wallace Gonçalves <wallacecosme@gmail.com>
2021-12-14 20:08:45 -0300

    Finalização do CRUD do Produto controller

--- ms-product/src/main/java/br/com/wgsolution/msproduct/controller/ProductController.java
+++ ms-product/src/main/java/br/com/wgsolution/msproduct/controller/ProductController.java
@@ -14,8 +14,12 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,36 +33,66 @@ public class ProductController {
 
 	@Autowired
 	private ProductService productService;
-	
+
 	@Autowired
 	private PagedResourcesAssembler<ProductDTO> assembler;
-	
-	@GetMapping(value = "/{id}", produces = {"application/json", "application/xml", "application/x-yml"})
+
+	@GetMapping(value = "/{id}", produces = { "application/json", "application/xml", "application/x-yml" })
 	public ProductDTO findById(@PathVariable("id") Long id) {
-		
+
 		ProductDTO productDTO = productService.findById(id);
 		productDTO.add(linkTo(methodOn(ProductController.class).findById(id)).withSelfRel());
-		
+
 		return productDTO;
 	}
-	
-	@GetMapping(produces = {"application/json", "application/xml", "application/x-yml"})
-	public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") int page, 
+
+	@GetMapping(produces = { "application/json", "application/xml", "application/x-yml" })
+	public ResponseEntity<?> findAll(@RequestParam(value = "page", defaultValue = "0") int page,
 			@RequestParam(value = "limit", defaultValue = "12") int limit,
-			@RequestParam(value = "direction", defaultValue = "0") String direction) {
-		
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+
 		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
-		
+
 		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "name"));
-		
+
 		Page<ProductDTO> products = productService.findAll(pageable);
-		
-		products.stream().forEach(p -> p.add(linkTo(methodOn(ProductController.class).findById(p.getId())).withSelfRel()));
-		
-		PagedModel<EntityModel<ProductDTO>> pagedModel = assembler.toModel(products);		
-		
+
+		products.stream()
+				.forEach(p -> p.add(linkTo(methodOn(ProductController.class).findById(p.getId())).withSelfRel()));
+
+		PagedModel<EntityModel<ProductDTO>> pagedModel = assembler.toModel(products);
+
 		return new ResponseEntity<>(pagedModel, HttpStatus.OK);
-		
+
+	}
+
+	@PostMapping(produces = { "application/json", "application/xml", "application/x-yml" }, consumes = {
+			"application/json", "application/xml", "application/x-yml" })
+	public ProductDTO create(@RequestBody ProductDTO productDTO) {
+
+		ProductDTO dto = productService.create(productDTO);
+		dto.add(linkTo(methodOn(ProductController.class).findById(dto.getId())).withSelfRel());
+
+		return dto;
+
+	}
+
+	@PutMapping(produces = { "application/json", "application/xml", "application/x-yml" }, 
+				consumes = {"application/json", "application/xml", "application/x-yml" })
+	public ProductDTO update(@RequestBody ProductDTO productDTO) {
+
+		ProductDTO dto = productService.update(productDTO);
+		dto.add(linkTo(methodOn(ProductController.class).findById(dto.getId())).withSelfRel());
+
+		return dto;
+
 	}
 	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id){
+		productService.delete(id);
+		
+		return ResponseEntity.ok().build();
+	}
+
 }
