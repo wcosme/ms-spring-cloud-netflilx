46b14c10d80ef322d99ce1f7650d72314cd18f07
Wallace Gonçalves <wallacecosme@gmail.com>
2022-04-11 21:12:07 -0300

    .

--- ms-payment/src/main/java/br/com/wgsolution/mspayment/entity/ProductSale.java
+++ ms-payment/src/main/java/br/com/wgsolution/mspayment/entity/ProductSale.java
@@ -2,14 +2,6 @@ package br.com.wgsolution.mspayment.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
-
-import lombok.Data;
-
-@Data
-@Entity
-@Table(name = "TB_PRODUCT_SALE")
 public class ProductSale implements Serializable{
 
 	private static final long serialVersionUID = 1L;
